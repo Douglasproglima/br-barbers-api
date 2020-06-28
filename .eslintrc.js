16e681e45d596a69f237a81c074d2df68b4e0d4c
Douglas Lima <douglasproglima@gmail.com>
2020-06-28 19:23:12 -0300

    Sequelize e Dialect BD

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,75 +1,23 @@
 module.exports = {
   env: {
-    es6: true,
+    es2020: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier'
-  ],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-          "singleQuote": true,
-          "printWidth": 120,
-          "trailingComma": "es5"
-      }
-    ],
-    "react/jsx-filename-extension": [
-      1,
-      {
-          "extensions": [
-              ".js",
-              ".jsx"
-          ]
-      }
-  ],
-  "react/prop-types": 0,
-  "no-unused-vars": [
-      "error",
-      {
-          "vars": "local",
-          "args": "none"
-      }
-  ],
-  "jsx-a11y/anchor-is-valid": [
-    "error",
-    {
-        "components": [
-            "Link"
-        ],
-        "specialLink": [
-            "to",
-            "hrefLeft",
-            "hrefRight"
-        ],
-        "aspects": [
-            "noHref",
-            "invalidHref",
-            "preferButton"
-        ]
-    }
-  ],
-    "no-console": "off",
-    "class-methods-use-this": "off",
-    "import/first": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off"
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
-  "extends": ["airbnb", "prettier", "prettier/react"]
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
 const routes = new Router();
+const users = ['Douglas', 'Fernando', 'Augusto'];
 
 routes.get('/', (req, res) => {
-  return res.json({ Message: 'Hello World - V1 API' });
+  return res.json(users);
+});
+
+routes.get('/users/:id', (req, res) => {
+  return res.json(users[req.params.id]);
+});
+
+routes.post('/users/', (req, res) => {
+  const name = req.body;
+  users.push(name);
+  return res.json(users);
 });
 
 export default routes;
