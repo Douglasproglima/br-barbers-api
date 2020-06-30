b0d5050d639b0730d3de15e4103e3261d1ce9f00
Douglas Lima <douglasproglima@gmail.com>
2020-06-29 22:25:19 -0300

    Add bcryptjs

--- src/database/seeds/20200630000941-users.js
+++ src/database/seeds/20200630000941-users.js
@@ -4,7 +4,8 @@ module.exports = {
       {
         name: 'Admin',
         email: 'admin@admin.com',
-        password_hash: '123456',
+        password_hash:
+          '$2a$08$GX7iZn1UitqQzS5zfIWTpuSz59ejrtCbUnpltluA3Gyw37tct9uxa',
         created_at: new Date(),
         updated_at: new Date(),
       },
