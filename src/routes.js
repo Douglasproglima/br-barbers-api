748777bee5dd8a3fb430486d5125b2aed6893c70
Douglas Lima <douglasproglima@gmail.com>
2020-06-30 23:38:18 -0300

    Feature Auth JWT

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
